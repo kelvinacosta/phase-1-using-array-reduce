f9be48f11446a2e7e56787dae7461b0980346b68
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-04-04 20:18:18 -0400

    complete reduce function'

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//New Variable totalBatteries
+//Initial Variable
+
+const initialValue = 0
+const totalBatteries = batteryBatches.reduce((accumulator,currentValue)=>{
+    return accumulator+currentValue
+},initialValue);
+
+
